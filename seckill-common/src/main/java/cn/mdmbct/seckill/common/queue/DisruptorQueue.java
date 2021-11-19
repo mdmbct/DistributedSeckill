031df4d2d83eb7652134c9cdc9a1de3659c2d84b
mdmbct <mdmbct@163.com>
2021-11-19 22:43:07 +0800

    updates.

---
+++ seckill-common/src/main/java/cn/mdmbct/seckill/common/queue/DisruptorQueue.java
@@ -0,0 +1,12 @@
+package cn.mdmbct.seckill.common.queue;
+
+/**
+ * Disruptor队列
+ *
+ * @author mdmbct  mdmbct@outlook.com
+ * @date 2021/11/19 22:25
+ * @modified mdmbct
+ * @since 0.1
+ */
+public class DisruptorQueue {
+}
